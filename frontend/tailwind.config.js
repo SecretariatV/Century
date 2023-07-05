c3fff19aaa899e4b908da57702b98052114e8548
padrone1225 <christoper.davis.25@gmail.com>
2023-07-05 18:42:28 +0900

    add loading screen

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#252526",
+        secondary: "#c6b68b",
+      },
+    },
   },
   plugins: [],
 };
