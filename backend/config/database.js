8f4d623f51b6d80d1b27709cd130c3c66df8e59d
SecretariatV <oliver.boucher.25@gmail.com>
2023-07-07 00:49:15 +0900

    change database post

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/century";
-// const MONGO_URI =
-//   process.env.MONGO_URI ||
-//   "mongodb+srv://kharchenkomykola9:DegdDNhMBs1E3pNa@cluster0.gggf7nu.mongodb.net/century";
+
+let MONGO_URI;
+
+if (process.env.NODE_ENV === "production") {
+  MONGO_URI =
+    process.env.MONGO_URI ||
+    "mongodb+srv://kharchenkomykola9:DegdDNhMBs1E3pNa@cluster0.gggf7nu.mongodb.net/century";
+} else {
+  MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/century";
+}
 
 const connectDatabase = () => {
   mongoose
