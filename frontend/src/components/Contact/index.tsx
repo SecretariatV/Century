9c072ea28fe69f2f01fd8083c17029b744a4e8dd
SecretariatV <oliver.boucher.25@gmail.com>
2023-07-06 22:24:19 +0900

    show office info

--- frontend/src/components/Contact/index.tsx
+++ frontend/src/components/Contact/index.tsx
@@ -4,6 +4,8 @@ import { buttonList } from "../../utils/contact";
 import Success from "../../../public/img/success.png";
 import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
 import { officeInfo } from "../../utils/map";
+import active from "../../../public/img/active.png";
+import dark from "../../../public/img/default.png";
 
 const Offices: Array<officeInfo> = [
   {
@@ -47,16 +49,12 @@ const Contact = () => {
   const [result, setResult] = useState<boolean>(false);
   const [officeInfo, setOfficeInfo] = useState<info>({
     address: "",
-    lat: -1,
-    lng: -1,
+    lat: 37.9422741,
+    lng: 23.6915615,
     phone: [],
     hour: "",
     email: "",
   });
-
-  useEffect(() => {
-    console.log("info", officeInfo);
-  }, [officeInfo]);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -108,15 +106,6 @@ const Contact = () => {
     });
   };
 
-  const customMarker = {
-    path: "M29.395,0H17.636c-3.117,0-5.643,3.467-5.643,6.584v34.804c0,3.116,2.526,5.644,5.643,5.644h11.759   c3.116,0,5.644-2.527,5.644-5.644V6.584C35.037,3.467,32.511,0,29.395,0z M34.05,14.188v11.665l-2.729,0.351v-4.806L34.05,14.188z    M32.618,10.773c-1.016,3.9-2.219,8.51-2.219,8.51H16.631l-2.222-8.51C14.41,10.773,23.293,7.755,32.618,10.773z M15.741,21.713   v4.492l-2.73-0.349V14.502L15.741,21.713z M13.011,37.938V27.579l2.73,0.343v8.196L13.011,37.938z M14.568,40.882l2.218-3.336   h13.771l2.219,3.336H14.568z M31.321,35.805v-7.872l2.729-0.355v10.048L31.321,35.805",
-    fillColor: "red",
-    fillOpacity: 2,
-    strokeWeight: 1,
-    rotation: 0,
-    scale: 1,
-  };
-
   return (
     <div className="absolute 2xl:top-100 top-80 left-1/2 -translate-x-1/2 bg-contact-primary rounded-[10px] grid gap-10 pt-19 pb-20.5 2xl:px-17.5 px-8 z-10 font-medium leading-[130%] uppercase">
       <div className="flex flex-col items-center justify-center">
@@ -132,21 +121,18 @@ const Contact = () => {
             <div className="flex flex-col gap-9">
               <div className="rounded-md bg-white h-[245px] xl:w-[492px] lg:w-[400px] w-[500px]">
                 {!isLoaded ? (
-                  <h1>Loading ...</h1>
+                  <></>
                 ) : (
                   <GoogleMap
                     mapContainerClassName="h-full w-full"
-                    // onLoad={onMapLoad}
-                    center={{ lat: 37.9422741, lng: 23.6915615 }}
-                    zoom={15}
+                    center={{ lat: officeInfo.lat, lng: officeInfo.lng }}
+                    zoom={12}
                   >
                     {markers.map(({ address, lat, lng, type }, id) => (
                       <MarkerF
                         key={type}
                         position={{ lat, lng }}
-                        icon={
-                          "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
-                        }
+                        icon={type === selectOffice ? active : dark}
                         onClick={() => {
                           handleMarkerClick(lat, lng, address, type, id);
                         }}
@@ -156,14 +142,24 @@ const Contact = () => {
                 )}
               </div>
               <div className="grid grid-cols-3 gap-[13px]">
-                {buttonList.map((item) => (
+                {buttonList.map((item, id) => (
                   <div
                     className={`cursor-pointer rounded-[5px] py-[11px] flex items-center justify-center hover:shadow-[0_0_16px_-3px_#BEB185,inset_0_0_9px_-3px_#b3a123bd] active:shadow-[0_0_10px_-3px_#BEB185,inset_0_0_15px_-3px_#b3a123bd] ${
                       selectOffice === item.type
                         ? "bg-contact-secondary text-white"
                         : "bg-transparent text-contact-secondary border border-contact-secondary"
                     }`}
-                    onClick={() => setSelectOffice(item.type)}
+                    onClick={() => {
+                      setSelectOffice(item.type);
+                      setOfficeInfo({
+                        address: markers[id].address,
+                        lat: markers[id].lat,
+                        lng: markers[id].lng,
+                        phone: Offices[id].phone,
+                        hour: Offices[id].hour,
+                        email: Offices[id].email,
+                      });
+                    }}
                     key={item.type}
                   >
                     <h1 className="text-xs leading-[14px]">paphos office</h1>
@@ -175,25 +171,27 @@ const Contact = () => {
               <div className="grid gap-8">
                 <div className="flex flex-col gap-1">
                   <h1 className="text-contact-secondary text-xs">address</h1>
-                  <h2 className="text-white">
-                    Taskalof 15, Kolonaki, Limassol, Cypruss, 22033
-                  </h2>
+                  <h2 className="text-white">{officeInfo.address}</h2>
                 </div>
                 <div className="flex flex-col gap-1">
                   <h1 className="text-contact-secondary text-xs">
                     business hours
                   </h1>
-                  <h2 className="text-white">Mon to Fri: 9:00 am - 6:00 pm</h2>
+                  <h2 className="text-white">{officeInfo.hour}</h2>
                 </div>
               </div>
               <div className="grid gap-8">
                 <div className="flex flex-col gap-1">
                   <h1 className="text-contact-secondary text-xs">phone</h1>
-                  <h2 className="text-white">{`+30 210 3635 457`}</h2>
+                  {officeInfo.phone.map((phone) => (
+                    <h2 className="text-white" key={phone}>
+                      {phone}
+                    </h2>
+                  ))}
                 </div>
                 <div className="flex flex-col gap-1">
                   <h1 className="text-contact-secondary text-xs">email</h1>
-                  <h2 className="text-white">info@century21.com</h2>
+                  <h2 className="text-white">{officeInfo.email}</h2>
                 </div>
               </div>
             </div>
